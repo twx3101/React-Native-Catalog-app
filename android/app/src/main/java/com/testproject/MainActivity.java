23ecc1149c8b9841468a15a4c5a03e206335fc46
Weixiong Tay <twx_3101@hotmail.com>
2018-12-19 14:13:00 +0800

    Changed app name

--- android/app/src/main/java/com/testproject/MainActivity.java
+++ android/app/src/main/java/com/testproject/MainActivity.java
@@ -27,3 +27,18 @@ public class MainActivity extends ReactActivity {
     };
   }
 }
+package com.testproject;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "TestProject";
+    }
+}
